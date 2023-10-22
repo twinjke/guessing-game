7245316c68285494d35a14098c93174d08bd5a1f
twinjke <seventeensasha7@gmail.com>
2023-10-22 12:52:01 +0300

    feat: add-games

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.number = Math.ceil((this.min + (this.max - this.min)/2));
+        return this.number
     }
 
     lower() {
-
+        this.max = this.number;
     }
 
     greater() {
-
+        this.min = this.number;
     }
 }
 
